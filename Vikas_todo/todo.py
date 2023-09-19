a90811d4742ce4dd589fdfeb4d6d7338bd099e8b
VIKAS VERMA <122379093+Vikasv06@users.noreply.github.com>
2023-09-19 15:10:25 +0530

    Add files via upload

---
+++ Vikas_todo/todo.py
@@ -0,0 +1,94 @@
+import tkinter
+from tkinter import *
+root = Tk()
+root.title("To Do List by Vikas")
+root.geometry("400x650+400+100")
+root.resizable(False,False)
+
+task_list=[]
+def addTask():
+    task= task_entry.get()
+    task_entry.delete(0,END)
+
+    if task:
+        with open("tasklist.txt",'a') as taskfile:
+            taskfile.write(f"\n{task}")
+        task_list.append(task)
+        listbox.insert(END,task)
+    
+def delete_Task():
+    global task_list
+    task= str(listbox.get(ANCHOR))
+    if task in task_list:
+        task_list.remove(task)
+        with open("tasklist.txt",'w') as taskfile:
+            for task in task_list:
+                taskfile.write(task+"\n")
+
+        listbox.delete(ANCHOR)
+
+def openTaskFile():
+    try:
+        global task_list
+
+        with open("tasklist.txt","r") as taskfile:
+            tasks = taskfile.readlines()
+
+        for task in tasks:
+            if task !='\n':
+                task_list.append(task)
+                listbox.insert(END,task)
+    except:
+        file = open('tasklist.txt', 'w')
+        file.close()
+
+#icons 
+Image_icon = PhotoImage(file="C:/Users/VIKAS VERMA/Desktop/Vikas_todo/Images/tasks.gif")
+root.iconphoto(False,Image_icon)
+
+#top bar
+TopImage = PhotoImage(file="C:/Users/VIKAS VERMA/Desktop/Vikas_todo/Images/toolbar1.png")
+Label(root,image=TopImage).pack()
+
+dockiImage = PhotoImage(file="C:/Users/VIKAS VERMA/Desktop/Vikas_todo/Images/dock.png")
+Label(root,image=dockiImage,bg="#32405b").place(x=30,y=25)
+
+noteImage = PhotoImage(file="C:/Users/VIKAS VERMA/Desktop/Vikas_todo/Images/tasks.gif")
+Label(root,image=noteImage,bg="#32405b").place(x=340,y=25)
+
+
+heading = Label(root,text="WRITE TASK",font="arial 20 bold", fg="white",bg="#2ddcde")
+heading.place(x=130,y= 20)
+
+#main
+frame = Frame(root,width=400, height=50,bg="white")
+frame.place(x=0,y=180)
+
+task=StringVar()
+task_entry= Entry(frame,width=18,font="arial 20",bd=0)
+task_entry.place(x=10,y=7)
+task_entry.focus()
+
+button = Button(frame,text="ADD",font="arial 20 bold",width=6,bg="#5a95ff",fg="#fff",bd=0, command=addTask)
+button.place(x=300,y=0)
+
+#listbox
+frame1= Frame(root,bd=3,width=700,height=280,bg="#2ddcde")
+frame1.pack(pady=(160,0))
+
+listbox = Listbox(frame1,font=('arial',12),width=40,height=16,bg="#2ddcde",fg="black",cursor="hand2",selectbackground="#474747")
+listbox.pack(side=LEFT, fill=BOTH, padx=2)
+scrollbar = Scrollbar(frame1)
+scrollbar.pack(side=RIGHT,fill=BOTH)
+
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
+
+openTaskFile()
+
+#delete
+Delete_icon=PhotoImage(file="C:/Users/VIKAS VERMA/Desktop/Vikas_todo/Images/delete.gif")
+Button(root,image=Delete_icon,bd=0,command=delete_Task).pack(side=BOTTOM,pady=13)
+
+
+root.mainloop()
